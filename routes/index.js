b8cfec34d04038cdfd3383f30e19a3236ea79e38
Qaalib Farah <[si8822fb@gmail.com]>
2019-02-03 10:16:07 -0600

    Added a bar chart displaying the top ten files in a chosen directory

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,30 @@ var express = require('express');
 var router = express.Router();
 var Disk = require('../models/localdisk');
 var dirname = 'C';
-var filesSize = require('filesize');
-var path = require('path');
+var topFiles;
+var polls = [
+  {
+    id:     1,
+    name:   "Will the Warriors win the NBA championship?",
+    is_featured: true
+  },
+  {
+    id:     2,
+    name:   "Will Trump win the GOP nomination?",
+    is_featured: false
+  }
+]
+console.dir(polls);
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Disk(function(err, diskData){
     if(err){
       res.render('error', {error: err})
-    }else{
+    }else if(topFiles){
+      console.log(topFiles);
+      res.render('index', {title: "Disk components", disk: diskData, objects: topFiles});
+    }
+    else{
       res.render('index', {title: "Disk components", disk: diskData});
     }
   });
@@ -21,8 +37,8 @@ router.get('/getDiskData', function(req, res, next){
   }, dirname)
 });
 router.post('/getDir', function(req, res, next){
-  console.log(req.body.files);
-
+  topFiles = req.body;
+  res.redirect('/');
 });
 
 module.exports = router;
