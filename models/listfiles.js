83fe487efdd5315ba21fb3c5de1f5a8b58fe01e5
Qaalib Farah <[si8822fb@gmail.com]>
2019-02-06 10:30:57 -0600

    added three charts pie, bar and line charts

--- models/listfiles.js
+++
@@ -1,5 +0,0 @@
-var dir = require('node-dir');
-
-function listFiles( dir.files()){
-
-}
--- routes/index.js
+++ routes/index.js
@@ -2,28 +2,13 @@ var express = require('express');
 var router = express.Router();
 var Disk = require('../models/localdisk');
 var dirname = 'C';
-var topFiles;
-var polls = [
-  {
-    id:     1,
-    name:   "Will the Warriors win the NBA championship?",
-    is_featured: true
-  },
-  {
-    id:     2,
-    name:   "Will Trump win the GOP nomination?",
-    is_featured: false
-  }
-]
-console.dir(polls);
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Disk(function(err, diskData){
-    if(err){
-      res.render('error', {error: err})
-    }else if(topFiles){
-      console.log(topFiles);
-      res.render('index', {title: "Disk components", disk: diskData, objects: topFiles});
+    if(err) {
+      res.render('error', {error: err});
     }
     else{
       res.render('index', {title: "Disk components", disk: diskData});
@@ -33,12 +18,7 @@ router.get('/', function(req, res, next) {
 router.get('/getDiskData', function(req, res, next){
   Disk(function(err, diskData){
     res.json(diskData);
-    console.log(diskData)
   }, dirname)
-});
-router.post('/getDir', function(req, res, next){
-  topFiles = req.body;
-  res.redirect('/');
 });
 
 module.exports = router;
